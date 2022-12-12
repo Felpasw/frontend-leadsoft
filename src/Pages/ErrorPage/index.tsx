d8c0583b0ca8ddce6d52a5d2e4d4aad973fe5663
Felipe Cavalcante Lacerda <felipecavalcantelacerda@hotmail.com>
2022-12-11 23:08:07 -0300

    Alteração da error page

--- src/Pages/ErrorPage/index.tsx
+++ src/Pages/ErrorPage/index.tsx
@@ -12,7 +12,7 @@ export default function ErrorPage(){
             <img src={require("../../@img/logoBranco.png")} alt="" style={{width: 400}}/>
             <p className="text-light" >ERROR <br />Parece que você não está autorizado para entrar aqui.</p>
             <p className="text-light" style={{fontSize:200}}><FaSadTear/></p>  
-            <button onClick={()=> navigate("/")} type="button" className="btn btn-outline-primary" > Back home</button> 
+            <button onClick={()=> navigate("/")} type="button" className="btn btn-outline-primary" > Voltar</button> 
         </div>
         <Footer/>
     </Fragment>
